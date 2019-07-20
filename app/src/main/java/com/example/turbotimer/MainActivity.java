6f8c7b2cc3b31340e60be8f6ccadab410c148e51
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2019-07-20 17:32:47 -0400

    countdown timer working

--- app/src/main/java/com/example/turbotimer/MainActivity.java
+++ app/src/main/java/com/example/turbotimer/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.turbotimer;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressBar countdownProgressBar;
+    TextView remainingTimeLabel;
+    Context context;
+    CountDownTimer timer;
+    Integer spoolDownTimeInSeconds = 70;
+    Integer spoolDownTimeInMilliseconds = spoolDownTimeInSeconds * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +36,60 @@ public class MainActivity extends AppCompatActivity {
         actionBar.hide();
 
         countdownProgressBar = findViewById(R.id.countdownProgress);
-        countdownProgressBar.setProgress(0, true);
+        remainingTimeLabel = findViewById(R.id.remainingTime);
+        context = getApplicationContext();
+
     }
 
-    public void startTimer(View v)
-    {
-        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
+    public void startTimer(View v) {
+
+        timer = new CountDownTimer(spoolDownTimeInMilliseconds, 333) {
+
+            public void onTick(long millisUntilFinished) {
+                Float progressValue = ((float)millisUntilFinished / spoolDownTimeInMilliseconds);
+                Log.d("Progress", progressValue.toString());
+                countdownProgressBar.setProgress(Math.round(progressValue*100), true);
+                String remainingTimeString = constructRemainingTimeLabel(millisUntilFinished);
+                setRemainingTimeLabel(remainingTimeString);
+            }
+
+            public void onFinish() {
+                countdownProgressBar.setProgress(100, true);
+                remainingTimeLabel.setText(R.string.timer_complete);
+                Toast.makeText(context, "Turbos have spooled down!", Toast.LENGTH_LONG).show();
+
+            }
+        }.start();
+
+        countdownProgressBar.setProgress(100, true);
+
+    }
+
+    private void setRemainingTimeLabel(String remainingTime) {
+        remainingTimeLabel.setText(remainingTime);
+    }
+
+    private String constructRemainingTimeLabel (long msRemaining) {
+        Integer minutes = getNumberOfMinutesRemaining(msRemaining);
+        Integer seconds = getNumberOfSecondsRemaining(msRemaining);
+        String secondsString;
+
+        if (seconds < 10) {
+            secondsString = "0" + seconds.toString();
+        } else {
+            secondsString = seconds.toString();
+        }
+
+        return minutes.toString() + ":" + secondsString;
+    }
+
+    private Integer getNumberOfMinutesRemaining(long msRemaining) {
+        Integer secondsRemaining =  (int) msRemaining / 1000;
+        return (int) Math.floor(secondsRemaining / 60);
+    }
+
+    private Integer getNumberOfSecondsRemaining(long msRemaining) {
+        Integer secondsRemaining = (int) msRemaining / 1000;
+        return secondsRemaining % 60;
     }
 }
